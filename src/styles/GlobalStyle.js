deacf4e1d9ac23a12a3a2b6f83ecf54a836bb6fc
dogsocks@naver.com <dogsocks@naver.com>
2022-05-09 22:38:17 +0900

    초기세팅 수정완료

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -3,7 +3,14 @@ import reset from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
-      
+  
+  * { 
+  box-sizing: border-box;
+}
+
+  body {
+  font-family: "Noto Sans KR", sans-serif;
+}
   `;
 
 export default GlobalStyle;
