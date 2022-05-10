174cc87999cd67f7836ee458b48d2120264b2597
leeheejuuun <94230809+leeheejuuun@users.noreply.github.com>
2022-05-10 14:57:46 +0900

    Add: card 컴포넌트 완성

---
+++ src/components/Card/cardData.js
@@ -0,0 +1,11 @@
+export const STACK_LIST = [
+  { id: 1, name: 'React', color: '#61DAFB' },
+  { id: 2, name: 'CSS', color: '#0C75B9' },
+  { id: 3, name: 'HTML', color: '#E54C25' },
+  { id: 4, name: 'JavaScript', color: '#FFD939' },
+  { id: 5, name: 'Python', color: '#3676AB' },
+  { id: 6, name: 'mysql', color: '#4579A1' },
+  { id: 7, name: 'django', color: '#0A2E20' },
+  { id: 8, name: 'git', color: '#F05032' },
+  { id: 9, name: 'scss', color: '#CF649A' },
+];
